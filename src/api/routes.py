8dc013348664f80924b330cb2f8f4ee66387ae07
Violeta Ventura <violetaventura@gmail.com>
2020-05-30 00:49:49 +0000

    completed

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,16 +4,23 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db
 from api.utils import generate_sitemap
+import random
 #from models import Person
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/excuse', methods=['GET'])
+def getrandomexecuse():
 
-    response_body = {
-        "hello": "world"
-    }
+    execuse = ("You wouldnt believe this but,", "OMG,", "Sweet baby jesus,")
+    who = ("The dog", "The nanny", "My Grandma")
+    action = ("ate", "ran over", "punched")
+    what = ("the homework", "the laptop", "the gardner")
+    when = ("yesterday", "today", "on friday")
 
-    return jsonify(response_body), 200
+    temporary = random.randint(0,2)
+    execuse= who[temporary]+" "+action[temporary] +" "+what[temporary]+" "+when[temporary]
+
+
+    return jsonify(execuse), 200
--- src/front/js/index.js
+++ src/front/js/index.js
@@ -5,6 +5,10 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
+window.onload = async function() {
   console.log("Hello Rigo from the console!");
+  const r = await fetch(
+    "https://3000-c53d1b0b-b53e-45cf-b9a5-bbb861d12eb9.ws-us02.gitpod.io/api/excuse"
+  );
+  document.querySelector(".alert").innerHTML = await r.json();
 };
